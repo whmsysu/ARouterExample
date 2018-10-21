a45b4cff270c770a954307d29ac23b9707aae790
Haomin Wu <wuhaominsysu@gmail.com>
2018-10-22 02:03:30 +0800

    Add httplibrary

---
+++ httplibrary/src/main/java/com/application/haominwu/httplibrary/util/ActivityCollector.java
@@ -0,0 +1,43 @@
+package com.application.haominwu.httplibrary.util;
+
+import android.app.Activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActivityCollector {
+
+    private static List<Activity> activities = new ArrayList<>();
+
+    /**
+     *
+     * @param activity
+     */
+    public static void pushActivity(Activity activity) {
+        activities.add(activity);
+    }
+
+    /**
+     *
+     * @param activity
+     */
+    public static void popActivity(Activity activity) {
+       activities.remove(activity);
+    }
+
+    /**
+     *
+     */
+    public static void clearAllActivities() {
+       activities.clear();
+    }
+
+    /**
+     *
+     * @return
+     */
+    public static Activity getTopActivity() {
+        return activities.get(activities.size() - 1);
+    }
+
+}
---
+++ httplibrary/src/main/java/com/application/haominwu/httplibrary/util/GsonUtil.java
@@ -0,0 +1,21 @@
+package com.application.haominwu.httplibrary.util;
+
+import com.google.gson.Gson;
+
+public class GsonUtil {
+
+    private GsonUtil(){
+
+    }
+
+    private static class GsonHolder{
+        private static final Gson INSTANCE = new Gson();
+    }
+
+
+    public static Gson getInstance()
+    {
+        return GsonHolder.INSTANCE;
+    }
+
+}
